de54cb0713342191391e972f50e1c1b337f01101
Amaldevm <amaldevhari265@gmail.com>
2024-11-20 22:43:42 +0530

    Initial commit

---
+++ src/layout/UserLayout.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import { Header } from "../components/user/Header";
+import { Footer } from "../components/user/Footer";
+import { Outlet } from "react-router-dom";
+import { UserHeader } from "../components/user/UserHeader";
+
+export const UserLayout = () => {
+    const [isUserAuth, setIsUserAuth] = useState(true);
+
+    return (
+        <div>
+            {isUserAuth ? <UserHeader /> : <Header />}
+            <div className="min-h-96">
+
+            <Outlet />
+            </div>
+            <Footer />
+        </div>
+    );
+};
