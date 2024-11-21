4298ca291b78a54dff49ba0bad1f51ee74a8dca6
Amaldevm <amaldevhari265@gmail.com>
2024-11-21 23:22:19 +0530

    crfeate components

--- src/components/user/Header.jsx
+++ src/components/user/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
+
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
+
+    return (
+    
+        <div className="flex justify-center items-center py-3 sm:py-4 shadow-lg sticky top-0 left-0 bg-black z-10 gap-3">
+
+        </div>
+    );
 }
 
 export default Header
+
+
+{/* <button className="inline-flex items-center justify-center rounded-xl bg-amber-500 py-3 px-6 font-dm text-base font-medium text-white shadow-lg transition-transform duration-200 ease-in-out hover:scale-[1.15]">
+    Join Us
+</button> */}
